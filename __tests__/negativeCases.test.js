17c58fc1b49c4471a86115255229771d5f5dd8e9
Mike <k2punk@gmail.com>
2022-08-19 13:48:14 +0300

    add beforeAll in tests

--- __tests__/negativeCases.test.js
+++ __tests__/negativeCases.test.js
@@ -1,9 +1,8 @@
-import { test, expect, beforeEach } from '@jest/globals';
+import { test, expect, beforeAll } from '@jest/globals';
 import * as path from 'path';
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import nock from 'nock';
-import os from 'os';
 import downloadPage from '../src/downloadPage.js';
 
 const { promises: fsp } = fs;
@@ -12,12 +11,11 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-let dir;
-
-beforeEach(async () => {
+beforeAll(async () => {
+  nock.disableNetConnect();
   nock('https://ru.hexlet.io')
     .get('/courses')
-    .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+    .reply(404, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
   nock('https://ru.hexlet.io')
     .get('/assets/professions/nodejs.png')
     .reply(200, await fsp.readFile(getFixturePath('nodejs.png')));
@@ -30,29 +28,9 @@ beforeEach(async () => {
   nock('https://ru.hexlet.io')
     .get('/packs/js/runtime.js')
     .reply(200);
-  dir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-});
-
-test('modify page', async () => {
-  await downloadPage('https://ru.hexlet.io/courses', dir);
-  const source = await fsp.readFile(getFixturePath('downloadedWithAliases.html'));
-  expect(source).toEqual(await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html')));
-});
-
-test('save file', async () => {
-  await downloadPage('https://ru.hexlet.io/courses', dir);
-  const file = await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html'), 'utf-8');
-  expect(file).not.toBeNull();
-});
-
-test('return right object', async () => {
-  const file = await fsp.readFile(getFixturePath('expected.json'), 'utf-8');
-  const object = JSON.parse(file);
-  expect(await downloadPage('https://ru.hexlet.io/courses', '/var/tmp')).toEqual(object);
 });
 
 test('network error', async () => {
-  nock.disableNetConnect();
   await expect(downloadPage('https://ru.hexlet.io/courses', '/usr')).rejects.toThrow();
 });
 
---
+++ __tests__/positiveCases.test.js
@@ -0,0 +1,81 @@
+import { test, expect, beforeAll, beforeEach } from '@jest/globals';
+import * as path from 'path';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+import nock from 'nock';
+import os from 'os';
+import downloadPage from '../src/downloadPage.js';
+
+const { promises: fsp } = fs;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+let dir;
+
+nock.disableNetConnect();
+
+beforeAll(async () => {
+  // nock.disableNetConnect();
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, await fsp.readFile(getFixturePath('nodejs.png')));
+  nock('https://ru.hexlet.io')
+    .get('/assets/application.css')
+    .reply(200, await fsp.readFile(getFixturePath('styles.css'), 'utf-8'));
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  nock('https://ru.hexlet.io')
+    .get('/packs/js/runtime.js')
+    .reply(200);
+// dir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+beforeEach(async () => {
+  // nock.disableNetConnect();
+  dir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+test('modify page', async () => {
+  // nock('https://ru.hexlet.io')
+  //   .get('/courses')
+  //   .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  // nock('https://ru.hexlet.io')
+  //   .get('/assets/professions/nodejs.png')
+  //   .reply(200, await fsp.readFile(getFixturePath('nodejs.png')));
+  // nock('https://ru.hexlet.io')
+  //   .get('/assets/application.css')
+  //   .reply(200, await fsp.readFile(getFixturePath('styles.css'), 'utf-8'));
+  // nock('https://ru.hexlet.io')
+  //   .get('/courses')
+  //   .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  // nock('https://ru.hexlet.io')
+  //   .get('/packs/js/runtime.js')
+  //   .reply(200);
+  await downloadPage('https://ru.hexlet.io/courses', dir);
+  const source = await fsp.readFile(getFixturePath('downloadedWithAliases.html'));
+  expect(source).toEqual(await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html')));
+});
+
+test('save file', async () => {
+  // nock('https://ru.hexlet.io')
+  //   .get('/courses')
+  //   .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  await downloadPage('https://ru.hexlet.io/courses', dir);
+  const file = await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html'), 'utf-8');
+  expect(file).not.toBeNull();
+});
+
+test('return right object', async () => {
+  // nock('https://ru.hexlet.io')
+  //   .get('/courses')
+  //   .reply(200, await fsp.readFile(getFixturePath('sourceWithAliases.html'), 'utf-8'));
+  const file = await fsp.readFile(getFixturePath('expected.json'), 'utf-8');
+  const object = JSON.parse(file);
+  expect(await downloadPage('https://ru.hexlet.io/courses', '/var/tmp')).toEqual(object);
+});
